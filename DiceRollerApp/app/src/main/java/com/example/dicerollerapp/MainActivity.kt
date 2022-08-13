c6c0cb0a2085eff9dc15b74dfce1d8e2d1630fd2
Montoya086 <mon21552@uvg.edu.gt>
2022-08-13 12:53:03 -0600

    Dice images

--- DiceRollerApp/app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ DiceRollerApp/app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.dicerollerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +12,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
+        rollButton.setOnClickListener { rollDice() }
+        //tirada inicial
+        rollDice()
+    }
+
+    /**
+     * Tira el dado y actualiza la pantalla con el resultado
+     */
+    private fun rollDice() {
+        //Crea un dado de 6 lados
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        //encuentra el imageView
+        val diceImage: ImageView=findViewById(R.id.imageView)
+        //determina la imagen de el dado
+        val drawableResource = when (diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        //actualiza la imagen
+        diceImage.setImageResource(drawableResource)
+        //actualiza la descripción de contenido
+        diceImage.contentDescription = diceRoll.toString()
+
+    }
+}
+class Dice (private val numSides: Int){
+    fun roll(): Int{
+        return (1..numSides).random()
     }
 }
