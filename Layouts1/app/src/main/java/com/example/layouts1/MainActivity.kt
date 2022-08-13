f335bc00a0d33a2a73504a52b1b45c673120a079
Montoya086 <mon21552@uvg.edu.gt>
2022-08-13 14:08:32 -0600

    Calculadora de propinas

--- Layouts1/app/src/main/java/com/example/layouts1/MainActivity.kt
+++ Layouts1/app/src/main/java/com/example/layouts1/MainActivity.kt
@@ -3,12 +3,31 @@ package com.example.layouts1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.layouts1.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.cos
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{calculateTip()}
+    }
+    fun calculateTip(){
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectId){
+            R.id.option_eighteen_percent -> 0.18
+            R.id.option_twenty_percent -> 0.20
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        if(binding.roundUpSwitch.isChecked){
+            tip=kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
